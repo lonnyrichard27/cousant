5e6fa63a4e3bca35f115468f25fdb8251d6b8c23
Oluwaloni Richard <lonnyrichard77@gmail.com>
2021-04-20 18:04:07 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,73 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+    <table id="customers">
+  <tr>
+    <th>Project Name</th>
+    <th>Developer</th>
+    <th>Main Contractor</th>
+    <th>Area</th>
+    <th>State</th>
+    <th>Status</th>
+    <th>Sector</th>
+  </tr>
+  <tr v-for="project in allProjects" :key="project.id">
+    <td>{{ project.project_name }}</td>
+    <td>{{ project.developer }}</td>
+    <td>{{ project.main_contractor }}</td>
+    <td>{{ project.lga }}</td>
+    <td>{{ project.state }}</td>
+    <td><span class="badge">{{ project.status }}</span></td>
+    <td>{{ project.sector }}</td>
+  </tr>
+</table>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  async mounted(){
+    const response = await this.$store.dispatch('fetchProjects', this.data)
+    console.log(response);
+  },
+  computed:{
+    allProjects(){
+      return this.$store.state.projects
     }
   }
+}
+</script>
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Questrial&display=swap');
+
+#customers {
+  font-family: 'Questrial', sans-serif;
+  font-size: 19px;
+
+font-weight: bold;
+  border-collapse: collapse;
+  width: 100%;
+}
+.badge{
+  background-color: #DFB97E;
+  color: #88621E;
+  padding: 5px;
+  border-radius: 5px
+}
+
+#customers td, #customers th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+#customers tr:nth-child(even){background-color: #FFFEFF;}
+
+#customers tr:hover {background-color: #ddd;}
+
+#customers th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #28394B;
+  color: white;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,28 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    projects: [],
   },
   mutations: {
+    SET_PROJECTS(state, projects) {
+      state.projects = projects
+    }
   },
   actions: {
+    async fetchProjects({ commit }){
+      try{
+        const {data} = await axios.get("https://244b8df3-7491-4cfd-a48b-267f19446372.mock.pstmn.io/")
+        commit('SET_PROJECTS', data.data)
+        return data
+      } catch (error){
+        console.log(error);
+      }
+    }
   },
   modules: {
   }
